135e0e3997fd20b4831ee8b2e2350372d7740fc3
Emily Runk <emily.runk01@gmail.com>
2018-02-20 13:53:28 -0800

    First bloopin' push Hello world project

---
+++ HelloWorld/src/com/emilyrunk/Main.java
@@ -0,0 +1,11 @@
+package com.emilyrunk;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Java!");
+        int sum = 2+2;
+        System.out.println("2 + 2 is " + sum);
+    }
+}
