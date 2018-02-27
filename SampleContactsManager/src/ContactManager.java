b5fce2c1661d030ba54cb6ab253e07468624df96
Emily Runk <emily.runk01@gmail.com>
2018-02-26 17:06:54 -0800

    Simple example contact manager via OOP

---
+++ SampleContactsManager/src/ContactManager.java
@@ -0,0 +1,26 @@
+public class ContactManager {
+    //Fields:
+    Contact [] myFriends;
+    int friendsCount;
+
+    //Constructor:
+    ContactManager(){
+        this.friendsCount = 0;
+        this.myFriends = new Contact[500];
+    }
+
+    void addContact(Contact contact){
+        myFriends[friendsCount] = contact;
+        friendsCount++;
+    }
+
+    Contact searchContact(String searchName){
+        for(int i=0; i<friendsCount; i++){
+            if(myFriends[i].name.equals(searchName)){
+                return myFriends[i];
+            }
+        }
+        return null;
+    }
+}
+
