7c7a7c4344294def4f0dce5e87173a0ce039f6ff
Emily Runk <emily.runk01@gmail.com>
2018-02-27 19:18:15 -0800

    Simple guessing game

---
+++ SampleNumberGuessingGame/src/SampleNumberGuessingGame.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SampleNumberGuessingGame {
+
+
+    public static void main(String [] args) {
+        int randomNumber = (int) (Math.random() * 100) + 1;
+        boolean hasWon = false;
+        System.out.println("I have randomly chosen a number between 1 and 100.");
+        System.out.println("Try to guess what it is!");
+
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 10; i > 0; i--) {
+            System.out.println("You have " + i + " guess(es) left. Choose again: ");
+            int guess = scanner.nextInt();
+
+            if (randomNumber < guess) {
+                System.out.println("It's smaller than " + guess + ".");
+            }
+
+            if (randomNumber > guess) {
+                System.out.println("It's greater than " + guess + ".");
+            }
+
+            if (randomNumber == guess) {
+                hasWon = true;
+                break;
+            }
+
+        }
+        if (hasWon) {
+            System.out.println("CORRECT... YOU WIN!!!");
+        } else {
+            System.out.println("GAME OVER... YOU LOSE!!!");
+            System.out.println("The number was : " + randomNumber);
+
+        }
+    }
+}
