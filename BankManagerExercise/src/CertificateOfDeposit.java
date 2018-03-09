38049500be3f272f0cf082aa3fdff104fa543f5b
Emily Runk <emily.runk01@gmail.com>
2018-03-09 12:32:39 -0800

    Bank Manager exercise that illustrates inheritance of Parent and Child classes

---
+++ BankManagerExercise/src/CertificateOfDeposit.java
@@ -0,0 +1,5 @@
+import java.util.Date;
+
+public class CertificateOfDeposit extends BankAccount {
+    Date expiry;
+}
---
+++ BankManagerExercise/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String [] args){
+        SavingsAccount svngs = new SavingsAccount();
+        CertificateOfDeposit COD = new CertificateOfDeposit();
+        CheckingAccount chking = new CheckingAccount();
+
+        svngs.balance = 5000;
+
+        COD.account = "1005";
+
+
+    }
+}
